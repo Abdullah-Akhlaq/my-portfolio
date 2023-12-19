ab78cf510073a939006892d07db4993f39cadecc
Abdullah-Akhlaq <abdullahakhlaq72@gmail.com>
2023-12-19 18:11:18 +0500

    update portolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate Front End Software Developer 🚀 having an experience of building different Type Website  with JavaScript / Reactjs / Nextjs  and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1uhWTCv3GLJRPCB0npVu-0B7LsO7InbJ4/view?usp=drive_link", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1DraVeOyyrINN1kyXpT1a5Ptoj08xAfaC/view?usp=drive_link", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
